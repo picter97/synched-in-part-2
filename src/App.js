958a8619d31f0ddc60c515e335b1382f183fb57f
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-04-02 11:00:25 -0400

    setup for part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+import fakeFetch from './utils/fakeFetch'
+
+import Header from './components/Header';
+import Person from './components/Person';
 
 function App() {
 
+  const [ people, setPeople ] = useState([])
+
+  useEffect(()=> {
+    const getPeople = async () => {
+      const res = await fakeFetch() // get data from "fetch"
+      setPeople(await res.json()) // store the result of .json() in state
+    }
+    getPeople()
+  },[])
+
   return (
-    <div className="App">
-      PLACEHOLDER!
+    <div className="app">
+      <Header />
+      <div className="people-div d-flex flex-wrap justify-content-center">
+          { people.map(person => <Person key={person.id} person={person} />) }
+      </div>
     </div>
   );
 }
---
+++ src/components/Person.jsx
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card'
+
+export default function Person (props) {
+
+    return (
+        <Card className="col-2 m-2 text-sm p-3">
+
+            <Card.Subtitle style={{borderBottom: "1px solid black"}}>{props.person.devLevel}</Card.Subtitle>
+            <Card.Title
+                className="mb-3 mt-3"
+                style={{ color: props.person.favoriteColor }} 
+            >
+                {props.person.fullName}
+            </Card.Title>
+            <Card.Subtitle>{props.person.company}</Card.Subtitle>
+            <Card.Text>
+                {props.person.bio}
+            </Card.Text>
+        </Card>
+    )
+
+}
